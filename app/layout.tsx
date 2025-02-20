cbf4c0ba1ca01b46e5e5f8cb51f1d978279d58c8
Kingsley Jide <kingsleyjide@yahoo.com>
2025-02-20 09:26:51 +0100

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Nexus',
+  description: 'Crypto Token',
+  generator: 'JIDNOC CONSULT',
 }
 
 export default function RootLayout({
